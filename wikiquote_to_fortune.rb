9af29e8570006c88d6429ea32fac4361567d1944
Lukas Zielinski <lukas@lukaszielinski.de>
2019-01-17 21:10:08 +0100

    Adds script.

---
+++ wikiquote_to_fortune.rb
@@ -0,0 +1,54 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+# Get name and make url
+
+if ARGV[0].nil?
+	puts "I need at least one parameter."
+	exit 1
+end
+
+page = ARGV[0]
+url = "https://en.wikiquote.org/w/index.php?action=edit&title=#{ARGV[0].gsub("\s","_")}"
+puts url
+
+# parse and extract quotes
+
+document = Nokogiri::HTML.parse(open(url))
+
+text = document.xpath("//textarea[@name='wpTextbox1']").text
+extract = text.scan(/^\*.*$/)
+
+# remove sources (start with **)
+
+extract.reject! do |line|
+	line =~ /^\*\*/
+end
+
+# remove mediawiki tags
+
+extract.collect! do |line|
+  line
+  	.gsub("''","")
+  	.gsub(/{{.*?}}/,"")
+  	.gsub(/<\/?[^>]+>/, "")
+  	.gsub(/^\*/,"")
+  	.gsub(/^\s/,"")
+  	.gsub(/\[\[w:/,"")
+  	.gsub(/\|.*?\]\]/, "")
+  	.gsub("[[","")
+  	.gsub("]]","")
+end
+
+# Leave out empty lines and list items that only contain urls
+
+extract.reject!{ |line| line =~ /^\[http.*/ }
+extract.reject!{ |line| line.length < 3}
+
+# Join and print
+
+joined = extract.join "\n%\n"
+
+puts joined
+
